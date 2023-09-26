bbd0048d94143328d864d44cae0b8d19e0d5db6d
Tanya <gusievatania2004@gmail.com>
2023-09-26 12:17:27 +0300

    Add project files.

---
+++ lab1/Deque.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Deque<T> : IEnumerable<T>
+    {
+        DequeElement<T> head;
+        DequeElement<T> tail;
+        int count;
+
+        // Adding the element
+        public void AddLast(T data)
+        {
+            DequeElement<T> node = new DequeElement<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+            tail = node;
+            count++;
+        }
+
+        public void AddFirst(T data)
+        {
+            DequeElement<T> node = new DequeElement<T>(data);
+            DequeElement<T> temp = head;
+            node.Next = temp;
+            head = node;
+            if (count == 0)
+                tail = head;
+            else
+                temp.Previous = node;
+            count++;
+        }
+
+        // Removing the element
+        public T RemoveFirst()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            T output = head.Data;
+            if (count == 1)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                head = head.Next;
+                head.Previous = null;
+            }
+            count--;
+            return output;
+        }
+
+        public T RemoveLast()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            T output = tail.Data;
+            if (count == 1)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                tail = tail.Previous;
+                tail.Next = null;
+            }
+            count--;
+            return output;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            DequeElement<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ lab1/DequeElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class DequeElement<T>
+    {
+        public DequeElement(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public DequeElement<T> Previous { get; set; }
+        public DequeElement<T> Next { get; set; }
+    }
+}
