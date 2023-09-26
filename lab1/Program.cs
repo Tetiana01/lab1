e20a27132e90faf3aede909930c5802f3563b353
Tanya <gusievatania2004@gmail.com>
2023-09-26 15:12:12 +0300

    Changes in the console app

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -16,11 +16,29 @@ namespace lab1
             foreach (int s in numbers)
                 Console.WriteLine(s);
 
+            Console.WriteLine("\nThe count of Deque: {0} \n", numbers.Count);
+
             int removedItem1 = numbers.RemoveFirst();
             Console.WriteLine("\nDelete the first: {0} \n", removedItem1);
 
             int removedItem2 = numbers.RemoveLast();
             Console.WriteLine("\nDelete the last: {0} \n", removedItem2);
+
+            Console.WriteLine("\nShow the new first element: {0} \n", numbers.First);
+
+            Console.WriteLine("\nShow the new last element: {0} \n", numbers.Last);
+            
+            Console.WriteLine("\nThe count of Deque: {0} \n", numbers.Count);
+
+            foreach (int s in numbers)
+                Console.WriteLine(s);
+
+            numbers.Clear();
+
+            Console.WriteLine("\nNew Deque: \n");
+
+            numbers.AddFirst(6);
+            numbers.AddLast(7);
 
             foreach (int s in numbers)
                 Console.WriteLine(s);
