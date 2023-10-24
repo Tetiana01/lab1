8611c9069ce6f4a030627eaf2677f8a6f218653e
Tanya <gusievatania2004@gmail.com>
2023-10-24 17:48:17 +0300

    All tests coverage

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -3,91 +3,58 @@ using Xunit;
 using lab1;
 using ClassLibrary1;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace TestProject1
 {
     public class UnitTest1
     {
-        [Fact]
+         [Fact]
         public void AddLast_IncreasesCount()
         {
-            // Arrange
             var deque = new DequeClass<int>();
 
-            // Act
             deque.AddLast(1);
 
-            // Assert
             Assert.Equal(1, deque.Count);
-        }
+        } 
 
         [Fact]
         public void RemoveFirst_DecreaseCount()
         {
-            //Arrange
             DequeClass<int> deque = new DequeClass<int>();
             deque.AddLast(1);
 
-            //Act
             deque.RemoveFirst();
 
-            //Assert
             Assert.Equal(0, deque.Count);
         }
 
         [Fact]
         public void First_ThrowsExceptionWhenEmpty()
         {
-            // Arrange
             var deque = new DequeClass<int>();
 
-            // Act and Assert
             Assert.Throws<InvalidOperationException>(() => deque.First);
         }
 
         [Fact]
         public void Last_ThrowsExceptionWhenEmpty()
         {
-            // Arrange
             var deque = new DequeClass<int>();
 
-            // Act and Assert
             Assert.Throws<InvalidOperationException>(() => deque.Last);
-        }
-
-        [Fact]
-        public void AddLastAndRemoveFirst_ReturnsCorrectElements()
-        {
-            // Arrange
-            var deque = new DequeClass<int>();
-
-            // Act
-            deque.AddLast(1);
-            deque.AddLast(2);
-            deque.AddLast(3);
-
-            int element1 = deque.RemoveFirst();
-            int element2 = deque.RemoveFirst();
-            int element3 = deque.RemoveFirst();
-
-            // Assert
-            Assert.Equal(1, element1);
-            Assert.Equal(2, element2);
-            Assert.Equal(3, element3);
         }
 
         [Fact]
         public void Clear_EmptiesTheDeque()
         {
-            // Arrange
             var deque = new DequeClass<char>();
             deque.AddLast('A');
             deque.AddLast('B');
 
-            // Act
             deque.Clear();
 
-            // Assert
             Assert.True(deque.IsEmpty);
             Assert.Equal(0, deque.Count);
         }
@@ -95,27 +62,82 @@ namespace TestProject1
         [Fact]
         public void Contains_ReturnsTrueForExistingElement()
         {
-            // Arrange
             var deque = new DequeClass<int>();
             deque.AddLast(10);
             deque.AddLast(20);
             deque.AddLast(30);
 
-            // Act and Assert
             Assert.True(deque.Contains(20));
         }
 
         [Fact]
         public void Contains_ReturnsFalseForNonExistingElement()
         {
-            // Arrange
             var deque = new DequeClass<string>();
-            deque.AddLast("Apple");
-            deque.AddLast("Banana");
-            deque.AddLast("Cherry");
+            deque.AddFirst("Apple");
+            deque.AddFirst("Banana");
+            deque.AddFirst("Cherry");
 
-            // Act and Assert
             Assert.False(deque.Contains("Grapes"));
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(10, 20, 30, 40, 50)]
+        public void AddLastAndRemoveFirst_ReturnsCorrectElements(params int[] elements)
+        {
+            var deque = new DequeClass<int>();
+
+            foreach (var element in elements)
+            {
+                deque.AddLast(element);
+            }
+
+            foreach (var element in elements)
+            {
+                Assert.Equal(element, deque.RemoveFirst());
+            }
+        }
+
+        [Theory]
+        [InlineData("a", "b", "c")]
+        [InlineData("w", "q", "y", "x", "z")]
+        public void AddFirstAndRemoveLast_ReturnsCorrectElements(params string[] elements)
+        {
+            var deque = new DequeClass<string>();
+
+            foreach (var element in elements)
+            {
+                deque.AddFirst(element);
+            }
+
+            foreach (var element in elements)
+            {
+                Assert.Equal(element, deque.RemoveLast());
+            }
+        }
+
+        [Fact]
+        public void GetEnumerator_ReturnsAllElements()
+        {
+            var deque = new DequeClass<int>();
+            deque.AddLast(1);
+            deque.AddLast(2);
+            deque.AddLast(3);
+
+            var elements = deque.ToList(); 
+
+            Assert.Equal(new List<int> { 1, 2, 3 }, elements);
+        }
+
+        [Fact]
+        public void GetEnumerator_EmptyDeque_ReturnsEmptyEnumeration()
+        {
+            var deque = new DequeClass<string>();
+
+            var elements = deque.ToList();
+
+            Assert.Empty(elements);
         }
     }
 }
